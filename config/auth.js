38d3da7975d4466d2f4592e63509404afa78a67a
al223ec <al223ec@student.lnu.se>
2015-01-14 19:24:19 +0100

    Callback och viewport

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '374626192709207', // your App ID
         'clientSecret'  : '5b92fefb6cbb882f0f8767e316d54b58', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'http://antonledstrom.se/auth/facebook/callback'
     },
 	'twitterAuth' : {
         'consumerKey'       : 'your-consumer-key-here',
