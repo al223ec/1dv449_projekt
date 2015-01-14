94e500f7821cb42cf9de1a4174df4d5ad996ecf8
al223ec <al223ec@student.lnu.se>
2015-01-14 19:37:33 +0100

    facebook

--- app/routes.js
+++ app/routes.js
@@ -40,10 +40,14 @@ module.exports = function(app, passport) {
     app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
     // handle the callback after facebook has authenticated the user
     app.get('/auth/facebook/callback',
-        passport.authenticate('facebook', {
-            successRedirect : '/profile',
-            failureRedirect : '/'
-        }));
+        passport.authenticate('facebook'), 
+        function(req, res) {
+            res.json({ 
+                loginOk: true,
+                user : req.user 
+            }); 
+        }
+    );
 
     app.get('/logout', function(req, res) {
         req.logout();
